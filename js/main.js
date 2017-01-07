b937f512986593f12a9b0fa2071a6425bdbe05b4
Chaos Predictor <kuzi81@gmail.com>
2017-01-08 01:34:18 +0200

    background image and text font change

--- js/main.js
+++ js/main.js
@@ -5,10 +5,9 @@ var lat,lng;
 $(document).ready(function(){
 	getLocation(usePosition);
 
-	//document.getElementById("send").addEventListener("click", function(){
-
-		//showPosition(lat,lng);
-		//googleGeocodeAPI(lat,lng);	});
+	document.getElementById("refresh").addEventListener("click", function(){
+			
+	});
 });
 
 function getLocation(callback) {
@@ -30,8 +29,6 @@ function usePosition(lat,lng) {
 
 
 function showPosition(lat,lng) {
-	//$("#demo-latitude").text("Latitude: " + lat);	
-	//$("#demo-longitude").text("Longitude: " + lng);
 }
 
 function showAddress(data) {
@@ -41,19 +38,17 @@ function showAddress(data) {
 }
 
 function showWeather(location) {
-	//console.log(location);
+
   $.simpleWeather({		
-    //location: 'Tel Aviv-Yafo, IL',
-    //location: 'Tel Aviv-Yafo, Israel',
     location: location,
     woeid: '',
     unit: 'c',
     success: function(weather) {
-      //html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
       $("#weather-temp").html('<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>');
       $("#weather-icon").html('<img src="'+weather.thumbnail+'">');
 			$("#weather-wind").html('<p>'+weather.wind.direction+' '+Math.round(weather.wind.speed)+' '+weather.units.speed+'</p>');
 			$("#weather-curr").html('<p>'+weather.currently+'</p>');
+			changeBackgroundImage(weather.currently);
     },
     error: function(error) {
       $("#weather-temp").html('<p> Error: '+error+'</p>');
@@ -62,14 +57,48 @@ function showWeather(location) {
   })
 }
 
+function changeBackgroundImage(curr) {
+  console.log(curr);	
+	switch(curr) {
+	case "Blowing Snow":
+		$('body').css('background-image','url(../img/Blowing-Snow.jpg)');
+		break;
+	case "Breezy":
+		$('body').css('background-image','url(../img/Breezy.jpg)');
+		break;
+	case "Clear":
+		$('body').css('background-image','url(../img/Clear.jpg)');
+		break;
+	case "Cloudy":
+		$('body').css('background-image','url(../img/Cloudy.jpg)');
+		break;
+	case "Mostly Sunny":
+		$('body').css('background-image','url(../img/Mostly-Sunny.jpg)');
+		break;
+	case "Partly Cloudy":
+	  console.log("this was run!");
+		$('body').css('background-image','url(img/Partly-Cloudy.jpg)');
+		break;
+	case "Partly Cloudy":
+		$('body').css('background-image','url(../img/Partly-Cloudy.jpg)');
+		break;
+	case "Rain":
+		$('body').css('background-image','url(../img/Rain.jpg)');
+		break;
+	case "Snow":
+		$('body').css('background-image','url(../img/Snow.jpg)');
+		break;
+	case "Snow Showers":
+		$('body').css('background-image','url(../img/Snow-Showers.jpg)');
+		break;
+	default:
+		$('body').css('background-color','red');
+		break;
+	}
+}
+
 function googleGeocodeAPI(lat,lng,callback){
-	//var lat = 40.714224;
-	//var lng = -73.961452;
-	//var data;
   $.post("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true", function(data){
-		//console.log(data);		
-		//return data; 
 		callback(data);     
-		//alert("Data: " + data.results[0].address_components[0].long_name);
   });
 }
