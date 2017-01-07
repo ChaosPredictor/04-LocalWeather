e26837835b35b94996a0c8c06585803d6d985a8b
Chaos Predictor <kuzi81@gmail.com>
2017-01-07 15:37:57 +0200

    reorganization of lat/lng parameters

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 var x = document.getElementById("demo");
 var http = new XMLHttpRequest();
+var lat,lng;
 
 $(document).ready(function(){
-	//getLocation();
+	getLocation();
 
 	document.getElementById("send").addEventListener("click", function(){
-		send();
-	});
+
+		showPosition(lat,lng);
+		googleGeocodeAPI(lat,lng);	});
 });
 
 function getLocation() {
 	navigator.geolocation.getCurrentPosition(function(location) {
 		if (location.coords) {
-			console.log(location.coords.latitude);
-  		console.log(location.coords.longitude);
-  		console.log(location.coords.accuracy);
-			showPosition(location.coords);
+			//console.log(location.coords.latitude);
+  		//console.log(location.coords.longitude);
+  		//console.log(location.coords.accuracy);
+			//showPosition(location.coords);
+			lat = location.coords.latitude;
+			lng = location.coords.longitude;
 		} else {
 			x.innerHTML = "Geolocation is not supported by this browser.";
 		}
@@ -23,17 +27,19 @@ function getLocation() {
 };
 
 
-function showPosition(position) {
-	$("#demo-latitude").text("Latitude: " + position.latitude);	
-	$("#demo-longitude").text("Longitude: " + position.longitude);
+function showPosition(lat,lng) {
+	$("#demo-latitude").text("Latitude: " + lat);	
+	$("#demo-longitude").text("Longitude: " + lng);
 }
 
 
-function send(){
-	var lat = 40.714224;
-	var lng = -73.961452;
-	var data;
+function googleGeocodeAPI(lat,lng){
+	//var lat = 40.714224;
+	//var lng = -73.961452;
+	//var data;
   $.post("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true", function(data){
-      alert("Data: " + data.results[0].address_components[0].long_name);
+		console.log(data);		
+		return data;      
+		//alert("Data: " + data.results[0].address_components[0].long_name);
   });
 }
